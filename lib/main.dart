287b26140165c9b7feafbf5f545ca5820fe484b0
jongmok.kim <a_mokkj@naver.com>
2020-01-19 18:26:10 +0900

    [dart] facebook test app

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,76 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Flutter Demo',
+  //     theme: ThemeData(
+  //       // This is the theme of your application.
+  //       //
+  //       // Try running your application with "flutter run". You'll see the
+  //       // application has a blue toolbar. Then, without quitting the app, try
+  //       // changing the primarySwatch below to Colors.green and then invoke
+  //       // "hot reload" (press "r" in the console where you ran "flutter run",
+  //       // or simply save your changes to "hot reload" in a Flutter IDE).
+  //       // Notice that the counter didn't reset back to zero; the application
+  //       // is not restarted.
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     home: MyHomePage(title: 'Flutter Demo Home Page'),
+  //   );
+  // }
+
+  void initiateFacebookLogin() async {
+    var facebookLogin = FacebookLogin();
+    var facebookLoginResult =
+        await facebookLogin.logIn(['email']);
+     switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        print("Error");
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("CancelledByUser");
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        print("LoggedIn");
+        onLoginStatusChanged(true);
+        break;
+    }
+  }
+
+  bool isLoggedIn = false;
+
+  void onLoginStatusChanged(bool isLoggedIn) {
+    // setState(() {
+      this.isLoggedIn = isLoggedIn;
+    // });
+  }
+    @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Facebook Login"),
+        ),
+        body: Container(
+          child: Center(
+            child: isLoggedIn
+                ? Text("Logged In")
+                : RaisedButton(
+                    child: Text("Login with Facebook"),
+                    onPressed: () => initiateFacebookLogin(),
+                  ),
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -66,6 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
